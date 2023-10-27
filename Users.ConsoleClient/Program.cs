761c99edcd2f5755ee65acedf4e1f89f635f57e1
Johnny Martinez <30609352+FrankMorro@users.noreply.github.com>
2023-10-27 14:13:26 -0400

    Agrega Proyecto MAUI

--- Users.ConsoleClient/Program.cs
+++ Users.ConsoleClient/Program.cs
@@ -2,7 +2,9 @@
 
 Host.Services.AddConsoleServices(
     Host.Configuration.GetSection("UsersEndpoints").Get<UsersEndpoints>());
+
 Host.Build();
 
 var view = Host.GetRequiredService<IUsersView>();
+
 await view.RenderUsersAsync();
---
+++ Users.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Users.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
